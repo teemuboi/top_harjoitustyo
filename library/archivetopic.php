ddaf9507c02cd0ac410bbee69e241d6ad85088e4
teemuboi <teemu.huttunen@edu.tampere.fi>
2021-11-23 14:29:31 +0200

    ajastus function testaamista

--- library/archivetopic.php
+++ library/archivetopic.php
@@ -3,42 +3,35 @@ require_once "../database/connection.php";
 require_once "../database/models/topics.php";
 require_once "../database/models/messages.php";
 function dateDifference($date){
-    return date('d', strtotime("now")-strtotime($date))-1;
+    $diff = strtotime($date)-time();
+    $days = round($diff/(60*60*24));
+    return -$days;
 }
+$topics = getAllTopics();
 
-function archive(){
-    $topics = getAllTopics();
-    
-    function date_sort($a, $b){
-        if(getMessageDate($b["topicid"]) || getMessageDate($a["topicid"])){
-            if(getMessageDate($b["topicid"]) && getMessageDate($a["topicid"])){
-                return strtotime(getMessageDate($b["topicid"])) - 
-                strtotime(getMessageDate($a["topicid"]));
-            }
-            if(getMessageDate($a["topicid"])){
-                return strtotime($b["date"]) - 
-                strtotime(getMessageDate($a["topicid"]));
-            }
-            if(getMessageDate($b["topicid"])){
-                return strtotime(getMessageDate($b["topicid"])) - 
-                strtotime($a["date"]);
-            }
+function date_sort($a, $b){
+    if(getMessageDate($b["topicid"]) || getMessageDate($a["topicid"])){
+        if(getMessageDate($b["topicid"]) && getMessageDate($a["topicid"])){
+            return strtotime(getMessageDate($b["topicid"])) - 
+            strtotime(getMessageDate($a["topicid"]));
         }
-
-        return strtotime($b["date"]) - strtotime($a["date"]);
-    }
-    usort($topics, "date_sort");
-
-
-    foreach($topics as $topic){
-        if(getMessageDate($topic["topicid"])){
-            $topic["date"] = getMessageDate($topic["topicid"]);
+        if(getMessageDate($a["topicid"])){
+            return strtotime($b["date"]) - 
+            strtotime(getMessageDate($a["topicid"]));
         }
-        if(dateDifference($topic["date"]) <= 10){
-            echo dateDifference($topic["date"])." ".$topic["title"]."<br>";
-        }else{
-            echo "deleted/archived"."<br>";
+        if(getMessageDate($b["topicid"])){
+            return strtotime(getMessageDate($b["topicid"])) - 
+            strtotime($a["date"]);
         }
     }
+
+    return strtotime($b["date"]) - strtotime($a["date"]);
 }
-archive();
+usort($topics, "date_sort");
+
+foreach($topics as $topic){
+    if(getMessageDate($topic["topicid"])){
+        $topic["date"] = getMessageDate($topic["topicid"]);
+    }
+    echo $topic["date"]." ".dateDifference($topic["date"])." ".$topic["title"]."<br>";
+}
