1a0c31730641caec7c92b6baab65d3ff575afa66
teemuboi <teemu.huttunen@edu.tampere.fi>
2021-11-24 14:30:45 +0200

    pieniä korjauksia ja muutoksia

--- library/archivetopic.php
+++ library/archivetopic.php
@@ -9,26 +9,6 @@ function dateDifference($date){
 }
 $topics = getAllTopics();
 
-function date_sort($a, $b){
-    if(getMessageDate($b["topicid"]) || getMessageDate($a["topicid"])){
-        if(getMessageDate($b["topicid"]) && getMessageDate($a["topicid"])){
-            return strtotime(getMessageDate($b["topicid"])) - 
-            strtotime(getMessageDate($a["topicid"]));
-        }
-        if(getMessageDate($a["topicid"])){
-            return strtotime($b["date"]) - 
-            strtotime(getMessageDate($a["topicid"]));
-        }
-        if(getMessageDate($b["topicid"])){
-            return strtotime(getMessageDate($b["topicid"])) - 
-            strtotime($a["date"]);
-        }
-    }
-
-    return strtotime($b["date"]) - strtotime($a["date"]);
-}
-usort($topics, "date_sort");
-
 foreach($topics as $topic){
     if(getMessageDate($topic["topicid"])){
         $topic["date"] = getMessageDate($topic["topicid"]);
--- views/topic.view.php
+++ views/topic.view.php
@@ -39,17 +39,15 @@ if($page-1 != 0){
 <table class="viewtopic">
 <?php foreach ($messages as $message) {?>
     <tr>
-        <td class="text"><i class="username"><?=getUser($message["userid"])["username"]?> <?=dateConvert($message["date"])?></i><div class="content"><?=censor_input(htmlentities($message["text"]))?></div></td>
-        <td class="info">
-            <?php if(isLoggedIn() && $_SESSION['userid'] == $message["userid"] || isAdmin()){?>
-                <?php if($_SESSION['userid'] == $message["userid"] || !isAdmin()){?>
-                    <a href='/editmessage?messageid=<?=$message["messageid"]?>'>edit</a>
-                <?php }?>
-                <a href='/deletemessage?messageid=<?=$message["messageid"]?>'>delete</a>
-            <?php }else{?>
-                <br>
+        <td class="text"><i class="username"><?=getUser($message["userid"])["username"]?> <?=dateConvert($message["date"])?></i>
+        <i class="operations">
+        <?php if(isLoggedIn() && $_SESSION['userid'] == $message["userid"] || isAdmin()){?>
+            <?php if($_SESSION['userid'] == $message["userid"] || !isAdmin()){?>
+                <a href='/editmessage?messageid=<?=$message["messageid"]?>'>edit</a>
             <?php }?>
-        </td>
+            <a href='/deletemessage?messageid=<?=$message["messageid"]?>'>delete</a>
+        <?php }?></i>
+        <div class="content"><?=censor_input(htmlentities($message["text"]))?></div></td>
         <td class="vote">
             <a <?=votedStyle($message["messageid"], 1)?> href='/vote?messageid=<?=$message["messageid"]?>&vote=upvote'>▲</a><br>
             <?=countMessageVotes($message["messageid"])?><br>
